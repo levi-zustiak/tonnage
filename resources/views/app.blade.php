0956d4f4120192b802b58f94daf82dc9c70ab62b
lzustiak <levizustiak@gmail.com>
2024-09-03 22:34:41 -0500

    feat: add frontend inertia setup

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -4,12 +4,12 @@
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0" />
 
-    <title>Tonnage</title>
+    <title>{{ config('app.name') }}</title>
 
     <link rel="preconnect" href="https://fonts.bunny.net">
     <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
 
-    @vite('resources/js/app.js')
+    @vite('resources/ts/app.tsx')
     @inertiaHead
 </head>
 <body>
