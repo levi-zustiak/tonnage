a1cf9432188e3ac9288f0a441913a6118d945b1e
lzustiak <levizustiak@gmail.com>
2024-09-06 23:24:22 -0500

    feat: add tailwind

--- resources/ts/app.tsx
+++ resources/ts/app.tsx
@@ -1,12 +1,14 @@
-import { createInertiaApp } from '@inertiajs/react'
-import { createRoot } from 'react-dom/client'
+import { createInertiaApp } from "@inertiajs/react";
+import { createRoot } from "react-dom/client";
+import '../css/app.css';
+import '../css/index.css';
 
 void createInertiaApp({
-    resolve: name => {
-        const pages = import.meta.glob('./Pages/**/*.tsx', { eager: true })
-        return pages[`./Pages/${name}.tsx`]
+    resolve: (name) => {
+        const pages = import.meta.glob("./Pages/**/*.tsx", { eager: true });
+        return pages[`./Pages/${name}.tsx`];
     },
     setup({ el, App, props }) {
-        createRoot(el).render(<App {...props} />)
+        createRoot(el).render(<App {...props} />);
     },
-})
+});
