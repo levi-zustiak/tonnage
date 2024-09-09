098dd95c980b461f8d4cf473d5fa7fd2f30773c6
lzustiak <levizustiak@gmail.com>
2024-09-08 23:43:48 -0500

    feat: setup file system

--- resources/ts/app.tsx
+++ resources/ts/app.tsx
@@ -1,12 +1,29 @@
-import { createInertiaApp } from "@inertiajs/react";
-import { createRoot } from "react-dom/client";
+import { createInertiaApp } from '@inertiajs/react';
+import { createRoot } from 'react-dom/client';
 import '../css/app.css';
 import '../css/index.css';
 
 void createInertiaApp({
-    resolve: (name) => {
-        const pages = import.meta.glob("./Pages/**/*.tsx", { eager: true });
-        return pages[`./Pages/${name}.tsx`];
+    resolve: (path) => {
+        const pages = import.meta.glob('./pages/**/*page.tsx', {
+            import: 'Page',
+            eager: true,
+        });
+
+        const layouts = Object.entries(import.meta.glob('./pages/'))
+            .filter(([file]) => {
+                const pattern = /\.\/pages|\/layout\.tsx/g;
+
+                return path.includes(file.replace(pattern, ''));
+            })
+            .sort(([keyA], [keyB]) => keyA.length - keyB.length)
+            .map(([file, layout]) => layout);
+
+        const page = pages[`./pages/${path}/page.tsx`];
+
+        // page.layout = layouts;
+
+        return page;
     },
     setup({ el, App, props }) {
         createRoot(el).render(<App {...props} />);
