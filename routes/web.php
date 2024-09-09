a136ed32318090d0215d04224c2a957aa04b64f8
lzustiak <levizustiak@gmail.com>
2024-09-08 23:50:26 -0500

    feat: add routes for auth flows

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,25 @@
 <?php
 
+use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\LoginController;
+use App\Http\Controllers\LogoutController;
+use App\Http\Controllers\RegisterController;
 use Illuminate\Support\Facades\Route;
+use Inertia\Inertia;
 
 Route::get('/', function () {
-    return view('welcome');
+    return Inertia::render('home');
+})->name('home');
+
+Route::middleware(['guest'])->group(function () {
+    Route::get('/login', [LoginController::class, 'index'])->name('login');
+    Route::post('/login', [LoginController::class, 'authenticate']);
+
+    Route::get('/register', [RegisterController::class, 'index'])->name('register');
+    Route::post('/register', [RegisterController::class, 'register']);
+});
+
+Route::middleware('auth')->group(function () {
+   Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
+   Route::get('/logout', LogoutController::class)->name('logout');
 });
