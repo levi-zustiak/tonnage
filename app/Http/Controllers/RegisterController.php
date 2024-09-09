f622fc3ca8914d32cfec79bc8ebf3369776aa6e1
lzustiak <levizustiak@gmail.com>
2024-09-08 23:49:30 -0500

    feat: add register functionality

---
+++ app/Http/Controllers/RegisterController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Inertia\Inertia;
+use Inertia\Response;
+
+class RegisterController extends Controller
+{
+    public function index(): Response
+    {
+        return Inertia::render('auth/register');
+    }
+
+    public function register(Request $request): RedirectResponse
+    {
+        $data = $request->validate([
+            'name' => ['required', 'string', 'max:255'],
+            'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
+            'password' => ['required', 'string', 'min:8'],
+//            'password' => ['required', 'string', 'min:8', 'confirmed'],
+        ]);
+
+        $user = User::create([
+            'name' => $data['name'],
+            'email' => $data['email'],
+            'password' => $data['password'],
+        ]);
+
+        Auth::login($user);
+
+        return to_route('dashboard');
+    }
+}
---
+++ resources/ts/pages/auth/register/page.tsx
@@ -0,0 +1,55 @@
+import { ReactElement } from 'react';
+import { SubmitHandler, useForm } from 'react-hook-form';
+import { TextField } from '@/components/TextField';
+import { router, usePage } from '@inertiajs/react';
+
+type FormData = {
+    name: string;
+    email: string;
+    password: string;
+};
+
+export function Page(): ReactElement {
+    const { errors: serverErrors } = usePage().props;
+    const {
+        register,
+        handleSubmit,
+        formState: { errors },
+    } = useForm<FormData>();
+
+    const onSubmit: SubmitHandler<FormData> = (data) =>
+        router.post('/register', data);
+
+    return (
+        <main>
+            <h1>Register</h1>
+            <form onSubmit={handleSubmit(onSubmit)}>
+                <div className="flex gap-4 card bg-base-100 p-4 bordered w-80">
+                    <TextField
+                        label="Name"
+                        {...register('name')}
+                        error={errors.name?.message ?? serverErrors.name}
+                    />
+                    <TextField
+                        label="Email"
+                        {...register('email')}
+                        error={errors.email?.message ?? serverErrors.email}
+                    />
+                    <TextField
+                        label="Password"
+                        {...register('password')}
+                        error={
+                            errors.password?.message ?? serverErrors.password
+                        }
+                    />
+                    <button
+                        className="btn btn-secondary w-full max-w-xs"
+                        type="submit"
+                    >
+                        Register
+                    </button>
+                </div>
+            </form>
+        </main>
+    );
+}
